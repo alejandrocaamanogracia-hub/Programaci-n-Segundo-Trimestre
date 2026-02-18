542f05f36e7e890c8e7e9bb38e2655ccdd01e773
Tek-H <carloscorchadobenito@gmail.com>
2026-02-18 16:55:02 +0100

    Añadiendo jornadas al torneo

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,9 +4,6 @@ import Clases.*;
 import Clases.competiciones.Torneo;
 import Clases.creacionObjetos.CreacionEquipos;
 import Clases.creacionObjetos.CreacionPersonas;
-import Clases.equipos.Equipo;
-import Clases.equipos.Estadio;
-import Clases.personas.Jugador;
 
 public class Main {
     public static void main(String[] args) {
